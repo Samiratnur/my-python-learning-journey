17bd8721011094681142a55a7b9660615fa36420
Samir Atnur <samiratnur0711@gmail.com>
2026-02-28 17:30:24 +0530

    Added 2026_work_Python folder

---
+++ 2026_work_Python/First.py
@@ -0,0 +1,10 @@
+def Num(a, b):
+    result = a + b
+    print("Addition ka result hai:", result)
+
+# User se input lena
+num1 = int(input("Pehla number likho: ")) 
+num2 = int(input("Dusra number likho: "))
+
+# Function call karna
+num(num1, num2)
